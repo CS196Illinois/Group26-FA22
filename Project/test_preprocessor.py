cdaa24a47a4ca59044951c5f33a50c8693e21c9e
dicarlo3 <dicarlo3@illinois.edu>
2022-11-09 08:37:05 -0600

    Wrote test files to check OS/env to determine     which preprocessor to run (and to test both)

--- Project/test_preprocessor.py
+++ Project/test_preprocessor.py
@@ -1,7 +1,14 @@
+from platform import system
+
 from linux_previous_to_next_preprocessor import LinuxPreProcess
-#from windows_previous_to_next_preprocessor import WindowsPreProcess
+from windows_previous_to_next_preprocessor import WindowsPreProcess
 
-preProcess = LinuxPreProcess()
-preProcess.runPreProcess("TrumpSpeeches")
+if (system() != 'Windows'):
+    preProcess = LinuxPreProcess()
+    preProcess.runPreProcess("TrumpSpeeches")
+    print(preProcess.serializeCurrentList("TrumpSpeeches", True))
 
-print(preProcess.serializeCurrentList("TrumpSpeeches", True))
+if (system() == 'Windows'):
+    preProcess = WindowsPreProcess()
+    preProcess.runPreProcess("TrumpSpeeches")
+    print(preProcess.serializeCurrentList("TrumpSpeeches", True))
